fd3f8c6a07deb93d4ed069fe1aa917380c1b7e21
endomorphosis <starworks5@gmail.com>
2024-04-20 13:01:29 -0700

    Update package name in package.json, add import statements in process_pins.py, and modify lines_per_file value in split.sh

--- add.sh
+++ add.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
-# 1 million line split using split command
+args=("$@")
+if [ $# -eq 0 ]; then
+    echo "Usage: ./add.sh <file>"
+    exit 1
+fi
+
+file_path=${args[0]}
+
+ipfs_cmd= ($(ipfs add -r $file_path --progress=false > ipfs_pins.tsv)) 
 
 counter=1
 lines_per_file=50000
-total_lines=$(wc -l < Caselaw_Access_Project_JSON_pins.tsv)
+total_lines=$(wc -l < ipfs_pins.tsv)
 
 while [ $counter -le $total_lines ]; do
     start_line=$counter
     end_line=$((counter + lines_per_file - 1))
     split_file="split-$counter-to-$end_line.tsv"
-    sed -n "${start_line},${end_line}p;${end_line}q" Caselaw_Access_Project_JSON_pins.tsv > $split_file
+    sed -n "${start_line},${end_line}p;${end_line}q" ipfs_pins.tsv > $split_file
     counter=$((counter + lines_per_file))
 done
+
 mkdir ipfs_pins
 mv split-*.tsv ipfs_pins
 node convert.js ipfs_pins
-rm Caselaw_Access_Project_JSON_pins.tsv
+rm ipfs_pins.tsv
