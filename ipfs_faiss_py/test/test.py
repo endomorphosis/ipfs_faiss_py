2e7770a4c15461140bf22ec6310c3ecb29d491c6
endomorphosis <starworks5@gmail.com>
2024-09-07 01:24:06 -0700

    Refactor test.py to load specific datasets and faiss indexes

--- ipfs_faiss_py/test/test.py
+++ ipfs_faiss_py/test/test.py
@@ -1,6 +1,10 @@
+import os
+import sys
 import datasets
 import ipfs_datasets_py
-from ..ipfs_embeddings_py import ipfs_embeddings_py
+parent_dir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(parent_dir)
+from ipfs_embeddings_py import ipfs_embeddings_py
 
 class test_ipfs_faiss:
     def __init__(self, resources, metadata):
@@ -11,20 +15,34 @@ class test_ipfs_faiss:
         self.ipfs_embeddings_py = ipfs_embeddings_py
         return None
     
-    def test_ipfs_faiss(self, dataset, faiss_index, elastic_index, join_column):
-        self.dataset = datasets.load_dataset("laion/Wikipedia-X")
-        self.faiss_index = datasets.load_faiss_index("laion/Wikipedia-M3")
-        print(self.faiss_index.columns)
-        self.elastic_index = datasets.load_elastic_index()
-        self.faiss_index.get_nearest_examples
+    def test_ipfs_faiss(self, dataset, faiss_index, elastic_index, join_column, test_query=None):
+        self.dataset = datasets.load_dataset(dataset)
+        print(self.dataset)
+        self.faiss_index = datasets.load_dataset(faiss_index)
+        print(self.faiss_index)
+        self.elastic_index = datasets.load_dataset(elastic_index)
+        print(self.elastic_index)
+        if test_query is not None:
+            self.test_query = [test_query]
+            self.query = self.test_query
+            self.ipfs_embeddings_py.add_tei_https_endpoint("BAAI/bge-m3", "62.146.169.111:80/embed",1)
+            self.ipfs_embeddings_py.add_tei_https_endpoint("BAAI/bge-m3", "62.146.169.111:8080/embed",1)
+            self.ipfs_embeddings_py.add_tei_https_endpoint("BAAI/bge-m3", "62.146.168.111:8081/embed",1)
         return None
     
 if __name__ == "__main__":
-    test_ipfs_faiss = test_ipfs_faiss()
+    resources = None
+    metadata = None
+    dataset = "laion/Wikipedia-X"
+    faiss_index = "laion/Wikipedia-M3"
+    elastic_index = None
+    join_column = "Version Control"
+    test_ipfs_faiss = test_ipfs_faiss(resources, metadata)
     try:
-        test_ipfs_faiss.test_ipfs_faiss()
+        test_ipfs_faiss.test_ipfs_faiss(dataset, faiss_index, elastic_index, join_column)
         print("test_ipfs_faiss passed")
-    except:
+    except Exception as e:
         print("test_ipfs_faiss failed")
+        print(e)
         exit(1)
     exit(0)
